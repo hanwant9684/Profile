8fd33387ea7a54ce525f4f2e1e5a25b91f37ee8b
Hanwant Beniwal <hanwant.beniwal9684@gmail.com>
2026-02-06 16:44:11 +0530

    Fix indentation for directory removal in main.py

--- main.py
+++ main.py
@@ -28,8 +28,8 @@ async def main():
             try:
                 if os.path.isfile(file_path) or os.path.islink(file_path):
                     os.unlink(file_path)
-                    elif os.path.isdir(file_path):
-                        shutil.rmtree(file_path)
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)
                 except Exception:
                     pass
 
