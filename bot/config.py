6a28f28d64d64248a5c972e181a6249ddaf9f4be
Hanwant Beniwal <hanwant.beniwal9684@gmail.com>
2026-04-08 09:02:38 +0530

    Add files via upload

--- bot/config.py
+++ bot/config.py
@@ -59,7 +59,7 @@ async def get_dump_channel_id():
     return os.environ.get("DUMP_CHANNEL_ID")
 
 # Performance Settings
-MAX_CONCURRENT_DOWNLOADS = 10
+MAX_CONCURRENT_DOWNLOADS = 20
 
 active_downloads = set()
 cancel_flags = set()       # Cancels the current single download item (used by /cancel)
@@ -108,5 +108,5 @@ app = Client(
     skip_updates=True,
     fetch_replies=0,
     max_concurrent_transmissions=20,
-    workers=10
+    workers=20
 )
