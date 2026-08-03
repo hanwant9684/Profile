56117b4a6131408f1fd48c9adefb5624cd69e578
Hanwant Beniwal <hanwant.beniwal9684@gmail.com>
2026-08-03 16:25:05 +0530

    Update transfer.py

--- bot/transfer.py
+++ bot/transfer.py
@@ -52,7 +52,7 @@ async def validate_bot_token(bot_token: str):
         bot_token=bot_token,
         in_memory=True,
         no_updates=True,
-        workers=100,
+        workers=15,
     )
     try:
         await asyncio.wait_for(probe.start(), timeout=20)
@@ -105,7 +105,7 @@ async def get_user_bot(user_id: int):
             in_memory=True,
             no_updates=True,
             sleep_threshold=30,
-            workers=100,
+            workers=15,
         )
         try:
             await asyncio.wait_for(client.start(), timeout=30)
